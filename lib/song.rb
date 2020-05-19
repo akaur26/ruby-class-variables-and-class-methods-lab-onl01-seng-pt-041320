48e98587adfa4c3c6d250f3579b25dc14b6cf0f5
Anmol Kaur <kaur.anmol26@gmail.com>
2020-05-19 00:30:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,12 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
+
+def initialize(song_name,artist,genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
 end
